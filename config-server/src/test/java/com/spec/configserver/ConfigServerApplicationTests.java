1f547b8cbdee8378ef5b351654171c04c4a3a0dd
a2w <a2w@localhost.localdomain>
2020-08-18 10:47:42 -0300

    add load balancer in payment + config server + zuul

---
+++ config-server/src/test/java/com/spec/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spec.configserver;
+
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
